0d34195a29774b75e813d8dd53f1854a8183484f
Younghun Choi <choi0hun@gmail.com>
2015-02-22 23:13:13 +0900

    bootstapping

---
+++ client/views/layout.js
@@ -0,0 +1,18 @@
+Accounts.ui.config({
+    requestPermissions: {},
+    extraSignupFields: [{
+        fieldName: 'username',
+        fieldLabel: 'username',
+        inputType: 'text',
+        visible: true,
+        saveToProfile: true
+    }]
+});
+
+Meta.config({
+  options: {
+    title: "SCAPE-VR",
+    suffix: "Suffix"
+  }
+});
+
