d0e3f3b537d4b4583699b9a9bf8eb876c44184b9
Younghun Choi <choi0hun@gmail.com>
2015-03-15 01:27:31 +0900

    bug fix

--- client/views/posts_show.js
+++ client/views/posts_show.js
@@ -108,7 +108,8 @@ Template.PostsShow.helpers({
   },
   "embedUrl": function(){
     var hrefList = location.href.split('/');
-    var address = location.protocol + "//" + hrefList[1] + hrefList[2] + "/ep/" + hrefList[4].split('?')[0];
+    console.log(location.href);
+    var address = location.protocol + "//" + hrefList[1] + hrefList[2] + "/ep/" + Router.current().params._id;
     return "<iframe width='560' height='315' src='" + address + "' frameborder='0' allowfullscreen></iframe>";
   }
 })
