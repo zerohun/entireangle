23b268efd78f13d89263d5319a0dd4992dfd78ef
Younghun Choi <choi0hun@gmail.com>
2015-03-18 18:03:36 +0900

    shareit version up

--- client/views/layout.js
+++ client/views/layout.js
@@ -27,3 +27,11 @@ Template.layout.helpers({
     return Meteor.user() != null;
   }
 })
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=1018333888196733&version=v2.0";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
