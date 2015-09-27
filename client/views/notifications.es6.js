2a132ee45b5b6e4c4ca31e532ea533dca8be5037
Younghoon Choi <choi0hun@gmail.comgit>
2015-09-27 18:36:58 +0900

    like notification and refactoring

--- client/views/notifications.es6.js
+++ client/views/notifications.es6.js
@@ -1,3 +1,19 @@
+function getReceiverName(user){
+  if(user._id === Meteor.userId())
+    return "your"
+  else
+    return user.username + "\'s";
+}
+
 Template.notifications.helpers({
   notifications: () => Notification.find({}, {sort: {createdAt: -1}})
 });
+
+Template.commentNotification.helpers({
+  receiverName: getReceiverName 
+});
+
+Template.likeNotification.helpers({
+  receiverName: getReceiverName 
+});
+
